9cdc50d944b0b16d056ed0a6833214a6c2279207
ayushkatiyar31 <ayushkatiyar827@gmail.com>
2026-03-06 14:54:02 +0530

    fix error

--- src/config/database.js
+++ src/config/database.js
@@ -1,8 +1,9 @@
 const mongoose = require("mongoose");
 
 const connectDB = async () => {
-  console.log(process.env.MONGO_URI);
-  await mongoose.connect(process.env.MONGO_URI);
+  console.log("Connecting to:", process.env.DB_CONNECTION_SECRET);
+  // Remove the options object - they're not needed in newer versions
+  await mongoose.connect(process.env.DB_CONNECTION_SECRET);
   console.log("Database connection established...");
 };
 
